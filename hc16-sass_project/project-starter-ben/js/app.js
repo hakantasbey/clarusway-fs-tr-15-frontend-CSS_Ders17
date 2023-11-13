a70ece8bc2f179c242a9745fc219148481c3580b
hakan <tasbeyhakan@gmail.com>
2023-11-13 14:03:44 +0300

    first commit

---
+++ hc16-sass_project/project-starter-ben/js/app.js
@@ -0,0 +1,9 @@
+//? JS
+
+const hamburgerBtn = document.querySelector(".fa-bars")
+
+
+hamburgerBtn.addEventListener("click", ()=>{
+    const headerLinks = document.querySelector(".header__links")
+    headerLinks.classList.toggle("show")
+})
